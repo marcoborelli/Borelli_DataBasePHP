e8614dab9d7d8bf253eaf1974795d6b287f58bee
marcoborelli <marco05.borelli@gmail.com>
2024-02-01 22:04:14 +0100

    Aggiunta navbar responsive

--- php/dipartimenti.php
+++ php/dipartimenti.php
@@ -17,9 +17,35 @@ $sheetNumber = 0;
     <link rel="stylesheet" type="text/css" href="../css/div.css">
     <title>HELO</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Borelli_DatabasePHP</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="dipartimenti.php">Dipartimenti</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="impiegati.php">Impiegati</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="partecipazioni.php">Partecipazioni</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="progetti.php">Progetti</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <div class="container">
         <div class="dataAndFilter">
             <div class="filter">
