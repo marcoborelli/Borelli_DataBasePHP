36605b7ed879f27e0aaaae2e911ecc6202ce731a
marcoborelli <marco05.borelli@gmail.com>
2024-02-01 20:34:30 +0100

    Implementato partecipazioni.php

---
+++ php/partecipazioni.php
@@ -0,0 +1,73 @@
+<html>
+<?php
+require_once('database.php');
+session_start();
+
+if (!isset($_SESSION["DATABASE"])) {
+    header("location:../html/login.html");
+    exit();
+}
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/div.css">
+    <title>HELO</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="dataAndFilter">
+            <div class="filter">
+                <form action="" method="POST">
+
+                    <label for="projNameInPartecipazioni">Nome progetto:</label>
+                    <input type="text" name="projNameInPartecipazioni"
+                        value=<?php echo (isset($_POST['projNameInPartecipazioni']) ? $_POST['projNameInPartecipazioni'] : "") ?>>
+
+                        <br>
+
+                    <label for="surnInPartecipazioni">Cognome dipendente:</label>
+                    <input type="text" name="surnInPartecipazioni" id="surnInPartecipazioni" value=<?php echo (isset($_POST['surnInPartecipazioni']) ? $_POST['surnInPartecipazioni'] : "") ?>>
+
+                    <br>
+
+                    <input type="submit" value="filterInDipartimenti">
+                    <!--<input type="submit" value="resetAllInDipartimenti" id="resetAllInDipartimenti">-->
+                </form>
+            </div>
+            <div class="insData">
+                
+            </div>
+        </div>
+        <div class="table" id="tabella">
+            <?php
+
+            $db = clone $_SESSION["DATABASE"];
+            if (
+                isset($_POST['projNameInPartecipazioni']) && isset($_POST['surnInPartecipazioni'])) {
+
+                $nomeProgetto = $_POST['projNameInPartecipazioni'] . "%";
+                $cognomeImpiegato = $_POST['surnInPartecipazioni'] . "%";
+
+                $query = $db->getBasicQuery(3);
+                $query .= " WHERE progetti.nome LIKE :projName AND impiegati.cognome LIKE :cognImp";
+
+                $tmpStatm = $db->getStatement($query);
+
+                $tmpStatm->bindParam(':projName', $nomeProgetto, PDO::PARAM_STR);
+                $tmpStatm->bindParam(':cognImp', $cognomeImpiegato, PDO::PARAM_STR);
+
+                echo $db->getTable(3, $db->executeQuery($tmpStatm));
+            } else {
+                echo $db->getBasicTable(3);
+            }
+
+            ?>
+        </div>
+    </div>
+
+</body>
+
+</html>
