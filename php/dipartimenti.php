11f14a404aa1c7bfed1d0979dabb72d3068c4ccb
Borelli Marco <Borelli.19312>
2024-02-02 11:34:21 +0100

    Cambiato titolo schede

--- php/dipartimenti.php
+++ php/dipartimenti.php
@@ -15,7 +15,7 @@ $sheetNumber = 0;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../css/div.css">
-    <title>HELO</title>
+    <title>DIPARTIMENTI</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
