6e55abab2ce2845760e4cfdeeae39a117dcd0e55
Borelli Marco <Borelli.19312>
2024-02-02 11:36:00 +0100

    La password è ora criptata

--- php/login.php
+++ php/login.php
@@ -3,10 +3,11 @@ require_once('database.php');
 
 session_start();
 $name = $_POST['user'];
-$password = $_POST['password'];
+$password = hash("sha512", $_POST['password']);
 
-if (($name == "programma" && $password == "123456") || ($name == "visualizzatore" && $password == "12345")) {
-    $database = Database::getDatbase($name, $password);
+if (($name == "programma" && $password == "ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413") 
+|| ($name == "visualizzatore" && $password == "12345")) {
+    $database = Database::getDatbase($name, $_POST['password']);
 
     $_SESSION["DATABASE"] = $database;
     //$_SESSION["PRIVILEGI_UTENTE"] = ($name == "programma") ? 1 : 0; //0 = visualizzatore, 1 = editor
